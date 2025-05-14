06c5252e6a303f3feaeb6ff97051552628dd93a3
Alejandra Torres Ugia <aletugi.to@gmail.com>
2025-05-14 13:18:25 +0200

    Initial Commit

---
+++ LoveBooks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LoveBooks
+//
+//  Created by Alejandra Torres on 14/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LoveBooks/LoveBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoveBooksApp.swift
+//  LoveBooks
+//
+//  Created by Alejandra Torres on 14/5/25.
+//
+
+import SwiftUI
+
+@main
+struct LoveBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
